f3871a84727d228ce32dbadc5f8f719a86df65bf
Angela Lima <angelamcostalima@icloud.com>
2022-10-14 19:43:06 +0100

    minor changes

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
 NAME	=	libftprintf.a
 
 CC	=		gcc
+
 CFLAGS	=	-Wall -Werror -Wextra
+
+LIBFT = libft
+
+HEADER = ft_printf.h -L./libft -lft
+
 RM	=		rm -f
 
 SRC	=		ft_itoa.c ft_printf.c ft_putchar_fd.c/
@@ -12,7 +18,10 @@ OBJ	=		$(SRC:.c=.o)
 all:		$(NAME)
 
 $(NAME):	$(OBJ)
+			make -C $(LIBFT)
+			cp libft/libft.a ./$(NAME)
 			ar rcs $(NAME) $(OBJ)
+			ranlib $(NAME)
 
 clean:
 			$(RM) $(OBJ)
--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 12:34:51 by anlima            #+#    #+#             */
-/*   Updated: 2022/10/14 19:10:05 by anlima           ###   ########.fr       */
+/*   Updated: 2022/10/14 19:42:13 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,6 @@
 # include <stdio.h>
 # include <stdarg.h>
 
-char	*ft_itoa(int n);
 int		ft_printf(const char *s, ...);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putstr_fd(char *s, int fd);
 
 #endif
