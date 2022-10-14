98eab97771dd8227dac227375ecd486b7031514b
Angela Lima <angelamcostalima@icloud.com>
2022-10-14 19:11:59 +0100

    add .h, make and a few aux functions

---
+++ Makefile
@@ -0,0 +1,23 @@
+NAME	=	libftprintf.a
+
+CC	=		gcc
+CFLAGS	=	-Wall -Werror -Wextra
+RM	=		rm -f
+
+SRC	=		ft_itoa.c ft_printf.c ft_putchar_fd.c/
+ft_putnbr_fd.c ft_putstr_fd.c
+
+OBJ	=		$(SRC:.c=.o)
+
+all:		$(NAME)
+
+$(NAME):	$(OBJ)
+			ar rcs $(NAME) $(OBJ)
+
+clean:
+			$(RM) $(OBJ)
+
+fclean:	clean
+			$(RM) $(NAME)
+
+re:	fclean	$(NAME)
---
+++ libftprintf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/13 12:34:51 by anlima            #+#    #+#             */
+/*   Updated: 2022/10/14 19:10:05 by anlima           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdarg.h>
+
+char	*ft_itoa(int n);
+int		ft_printf(const char *s, ...);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+
+#endif
