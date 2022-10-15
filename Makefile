8093f1509f8df4808b8b915f8151b5b2578ed960
Angela Lima <angelamcostalima@icloud.com>
2022-10-15 11:12:22 +0100

    change header and make to use libft functions

--- Makefile
+++ Makefile
@@ -4,24 +4,37 @@ CC	=		gcc
 
 CFLAGS	=	-Wall -Werror -Wextra
 
+LIBFT = 	libft
+
+HEADER = 	ft_printf.h -L./libft -lft
+
 RM	=		rm -f
 
-HEADER = 	 ft_printf.h
+HEADER = 	ft_printf.h
 
-SRC	=		ft_printf.c ft_itoa.c ft_putchar_fd.c\
-ft_putnbr_base.c ft_putnbr_fd.c ft_putstr_fd.c
+SRC	=		ft_printf.c ft_putnbr_base.c
 
 OBJ	=		$(SRC:.c=.o)
 
 all:		$(NAME)
 
 $(NAME):	$(OBJ)
+			make -C $(LIBFT)
+			cp libft/libft.a ./$(NAME)
 			ar rcs $(NAME) $(OBJ)
+			ranlib $(NAME)
+
+.o.c:
+	@$(CC) $(CFLAGS) -I $(HEADER) -o -c
 
 clean:
 			$(RM) $(OBJ)
+			make clean -C $(LIBFT)
 
 fclean:	clean
 			$(RM) $(NAME)
+			make fclean -C $(LIBFT)
 
-re:	fclean	$(NAME)
+re:	fclean	all
+
+.PHONY: all clean fclean re
--- ft_printf.h
+++ ft_printf.h
@@ -6,23 +6,16 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 12:34:51 by anlima            #+#    #+#             */
-/*   Updated: 2022/10/15 10:14:53 by anlima           ###   ########.fr       */
+/*   Updated: 2022/10/15 11:03:56 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-# include <unistd.h>
-# include <stdio.h>
 # include <stdarg.h>
-# include <stdlib.h>
-# include <string.h>
+# include "libft/libft.h"
 
-char	*ft_itoa(int n);
 int		ft_printf(const char *s, ...);
-void	ft_putchar_fd(char c, int fd);
 void	ft_putnbr_base(int nbr, char *base, int i);
-void	ft_putnbr_fd(unsigned int n, int fd);
-void	ft_putstr_fd(char *s, int fd);
 
 #endif
