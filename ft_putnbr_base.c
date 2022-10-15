3a520c5a155092631099094474a681e023cc7262
Angela Lima <angelamcostalima@icloud.com>
2022-10-15 11:51:57 +0100

    fix putnbr_base

--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -6,19 +6,23 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 19:28:55 by anlima            #+#    #+#             */
-/*   Updated: 2022/10/15 10:15:24 by anlima           ###   ########.fr       */
+/*   Updated: 2022/10/15 11:24:29 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_putnbr_base(int nbr, char *base, int i)
+void	ft_putnbr_base(int n, char *base, int i)
 {
-	if (nbr < 0)
+	unsigned int	nbr;
+
+	if (n < 0)
 	{
 		write(1, "-", 1);
-		nbr = -nbr;
+		nbr = -n;
 	}
+	else
+		nbr = n;
 	if (nbr / i > 0)
 		ft_putnbr_base(nbr / i, base, i);
 	ft_putchar_fd(base[nbr % i], 1);
